08b222b6dfdaaeca1e5baf728372220478026201
Noah Coe <loosive@Noahs-MacBook-Pro.local>
2023-12-30 22:13:43 -0500

    this is a test test test

---
+++ eggs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//battleship
+
+/*
+vim
+vigor
+*/
+int main()
+{
+
+	printf("How many eggs yo: ");
+
+	int  eggs;
+	scanf("%i", &eggs);
+
+	double dozen = (double) eggs / 12;
+
+	printf("You have %f dozen eggs.\n", dozen);
+
+	return 0;
+
+}
